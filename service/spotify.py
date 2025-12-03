ebed0b6f1ae29be29928aea391a92530187f248e
David Cánovas <davidnovas2003@gmail.com>
2025-12-03 20:47:15 +0100

    initial commit

---
+++ service/spotify.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+load_dotenv()
+import os
+import requests
+
+def get_user_data(user_id: str):
+    API_KEY_Public = os.getenv("client_Public_Key")
+    API_KEY_Secret = os.getenv("client_Secret_Key")
+
+    
+    url_API_Spotify = 'https://accounts.spotify.com/api/token'
+    header_Spotify = {"Content-Type": "application/x-www-form-urlencoded"}
+    data_Spotify = {
+                "grant_type": "client_credentials",
+                "client_id": API_KEY_Public,
+                "client_secret": API_KEY_Secret
+                }
+    auth_response = requests.post(url_API_Spotify, headers = header_Spotify, data = data_Spotify)
+    access_token = auth_response.json()['access_token']
+    print(access_token)
+    headers = {"Authorization": f"Bearer {access_token}"}
+    url = f"https://api.spotify.com/v1/users/{user_id}"
+    response = requests.get(url, headers=headers)
+    user_data = response.json()
+    if response.status_code != 200:
+        return{"error": "User not found"}
+    return user_data
---
+++ spotifyMain.py
@@ -0,0 +1,78 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+import requests
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import JSONResponse
+import mysql.connector
+from configuration.conections import DatabaseConnection
+import os
+from service.spotify import get_user_data
+
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"Hello world"}
+
+@app.get("/users")
+async def get_users():
+    mydb  = DatabaseConnection(host="localhost", user="root", password="Daviidcanovasss_300", database="spotifyProject")
+    mydb_conn = await mydb.get_connection()
+    mycursor = mydb_conn.cursor()
+    mycursor.execute("SELECT * FROM users")
+    data = mycursor.fetchall()
+    return data
+
+@app.post("/users")
+async def post_users(request: Request):
+    mydb  = DatabaseConnection(host="localhost", user="root", password="Daviidcanovasss_300", database="spotifyProject")
+    mydb_conn = await mydb.get_connection()
+    request = await request.json()
+    username = request.get('username')
+    favouriteArtist = request.get('favourite_artist')
+    mycursor = mydb_conn.cursor()
+    mycursor.execute(f"INSERT INTO users (username, favourite_artist) VALUES ('{username}', '{favouriteArtist}')")
+    mydb_conn.commit()
+    return JSONResponse(content={"message": "User added susccesfully"}, status_code = 201)
+   
+
+@app.delete("/users")
+async def delete_user(id_user: int):
+    mydb  = DatabaseConnection(host="localhost", user="root", password="Daviidcanovasss_300", database="spotifyProject")
+    mydb_conn = await mydb.get_connection()
+    mycursor = mydb_conn.cursor()
+    mycursor.execute(f"SELECT id FROM users WHERE id = {id_user}")
+    data = mycursor.fetchall()
+    if data is None:
+        return JSONResponse(content={"message": "User not found"})
+    else:
+        mycursor.execute(f"DELETE FROM users WHERE id = {id_user}")
+        mydb_conn.commit()
+        return JSONResponse(content={"message": "User deleted susccesfully"})
+    
+
+@app.put("/users/{user_id}")
+async def put_user(user_id: int, request: Request):
+    mydb  = DatabaseConnection(host="localhost", user="root", password="Daviidcanovasss_300", database="spotifyProject")
+    mydb_conn = await mydb.get_connection()
+    mycursor =  mydb_conn.cursor()
+    user_data = await request.json()
+    username = user_data.get("username")
+    favourite_artist = user_data.get("favourite_artist")
+    mycursor.execute(f"SELECT id FROM users WHERE id = {user_id}")
+    data = mycursor.fetchall()
+    if data is None:
+        return JSONResponse(content={"message": "User not found"})
+    else:
+        mycursor.execute(f"UPDATE users SET username = '{username}', favourite_artist = '{favourite_artist}' WHERE id = {user_id}")
+        mydb_conn.commit()
+        return JSONResponse(content={"message": "User updated susccesfully"})
+
+@app.get("/users/{user_id}")
+async def get_data(user_id: str):
+    user_data = get_user_data(user_id)
+    if "error" in user_data:
+        raise HTTPException(status_code=404, detail="User not found")
+    return JSONResponse(content=user_data, status_code=200)
